ec837bd6ad90668b8aad1f73909dee11a1539c75
Damian <d.pagowski@gmail.com>
2019-10-08 18:57:11 +0700

    first version of timestamp microservice

---
+++ README.md
@@ -0,0 +1,18 @@
+## Description
+Timestamp Microservice
+## Running local
+
+# .env file
+create .env file in project root directory.
+In .env file specify port number. For example:
+PORT=3000
+
+# installing project
+npm i
+npm start
+
+
+## Example Usage
+
+GET /api/timestamp/2019-10-8
+GET /api/timestamp/1570535473672
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const port = process.env.PORT; 
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  const dateString = req.params["date_string"];
+  const timestamp = parseInt(dateString);
+  const date =
+    isNaN(timestamp) == false ? new Date(timestamp) : new Date(dateString);
+  if (isNaN(date) == false) {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
