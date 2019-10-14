78a125a45c39a1a1a4a973dc0821dc8952339d5f
Damian <d.pagowski@gmail.com>
2019-10-14 21:08:16 +0700

    added UI

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-require("dotenv").config();
-const port = process.env.PORT; 
+require('dotenv').config()
+const port = process.env.PORT
 
-app.get("/api/timestamp/:date_string", (req, res) => {
-  const dateString = req.params["date_string"];
-  const timestamp = parseInt(dateString);
+app.use('/public', express.static(process.cwd() + '/public'))
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
+
+app.get('/api/timestamp/:date_string', (req, res) => {
+  const dateString = req.params['date_string']
+  const timestamp = parseInt(dateString)
   const date =
-    isNaN(timestamp) == false ? new Date(timestamp) : new Date(dateString);
+    isNaN(timestamp) == false ? new Date(timestamp) : new Date(dateString)
   if (isNaN(date) == false) {
-    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    res.json({ unix: date.getTime(), utc: date.toUTCString() })
   } else {
-    res.json({ error: "Invalid Date" });
+    res.json({ error: 'Invalid Date' })
   }
-});
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
